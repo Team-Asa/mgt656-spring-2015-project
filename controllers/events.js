a8f662f2a8057366d7b190c427fb62b80ed09baa
Stephen Hemenway <stephen.hemenway@yale.edu>
2015-02-08 18:07:54 +0000

    add api to see events

--- controllers/events.js
+++ controllers/events.js
@@ -147,6 +147,11 @@ function rsvp (request, response){
 
 }
 
+function api (request, response){
+  var output = {events: events.all};
+  response.send(output);
+}
+
 /**
  * Export all our functions (controllers in this case, because they
  * handles requests and render responses).
@@ -156,5 +161,6 @@ module.exports = {
   'eventDetail': eventDetail,
   'newEvent': newEvent,
   'saveEvent': saveEvent,
-  'rsvp': rsvp
+  'rsvp': rsvp,
+  'api': api
 };

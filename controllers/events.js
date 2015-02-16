31850ebf50981db1fa206d4660b3333688bf80f3
Stephen Hemenway <stephen.hemenway@yale.edu>
2015-02-16 01:45:00 +0000

    event detail page

--- controllers/events.js
+++ controllers/events.js
@@ -136,12 +136,17 @@ function rsvp (request, response){
     response.status(404).send('No such event');
   }
 
-  if(validator.isEmail(request.body.email)){
+  if(validator.isEmail(request.body.email) && request.body.email.toLowerCase().indexOf('yale.edu') !== -1){
     ev.attending.push(request.body.email);
     response.redirect('/events/' + ev.id);
   }else{
     var contextData = {errors: [], event: ev};
-    contextData.errors.push('Invalid email');
+    if(request.body.email.toLowerCase().indexOf('harvard') !== -1){
+      contextData.errors.push('No Harvardians');
+    }
+    else{
+      contextData.errors.push('Invalid email');
+    }
     response.render('event-detail.html', contextData);    
   }
 

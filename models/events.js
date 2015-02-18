47d8d0cee975608a249e17d5714f5d293c184b45
Anthony Nichols <anthony.nichols@yale.edu>
2015-02-18 18:57:40 +0000

    img changes

--- models/events.js
+++ models/events.js
@@ -11,7 +11,7 @@ var allEvents = [
     // so, month zero is January. This is Jan 17th
     // 2013 at 4:30pm local time.
     date:   new Date(2016, 0, 17, 16, 30, 0),
-    image: 'http://i.imgur.com/pXjrQ.gif',
+    image: 'http://static.fjcdn.com/gifs/Party_6f9750_2098765.gif',
     location: 'Kyle \'s house',
     attending: ['kyle.jensen@yale.edu', 'kim.kardashian@yale.edu']
   },
@@ -19,7 +19,7 @@ var allEvents = [
     id: 1,
     title: 'BBQ party for hackers and nerds',
     date:   new Date(2015, 8, 1, 19, 0, 0),
-    image: 'http://i.imgur.com/7pe2k.gif',
+    image: 'http://demolitionparts.com/wp-content/uploads/IMG_9788LR.jpg',
     location: 'Miles\' house',
     attending: ['kyle.jensen@yale.edu', 'kim.kardashian@yale.edu']
   },
@@ -27,7 +27,7 @@ var allEvents = [
     id: 2,
     title: 'BBQ for managers',
     date:   new Date(2015, 9, 20, 18, 0, 0),
-    image: 'http://i.imgur.com/CJLrRqh.gif',
+    image: 'http://i.imgur.com/hoIb3AZ.gif',
     location: 'Barry Nalebuff\'s house',
     attending: ['kim.kardashian@yale.edu'],
   },
@@ -35,7 +35,7 @@ var allEvents = [
     id: 4,
     title: 'Cooking lessons for the busy business student',
     date:   new Date(2014, 8, 2, 19, 0, 0),
-    image: 'http://i.imgur.com/02KT9.gif',
+    image: 'http://s3-ec.buzzfed.com/static/enhanced/webdr03/2013/2/7/15/anigif_enhanced-buzz-26250-1360270013-13.gif',
     location: 'Yale Farm',
     attending: ['homer.simpson@yale.edu'],
   }
